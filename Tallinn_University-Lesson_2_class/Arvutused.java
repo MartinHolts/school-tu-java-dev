ec47b584f68caa463070a4f387105ad1eed4c185
MartinHoltsmeier <martinholtsmeier@gmail.com>
2023-01-06 21:04:09 +0200

    Commented straight triangle exercises.

--- Tallinn_University-Lesson_2_class/Arvutused.java
+++ Tallinn_University-Lesson_2_class/Arvutused.java
@@ -11,14 +11,24 @@ public class Arvutused {
 
 	public static void main(String[] args) {
 		System.out.println("Antud programm aitab leida taisnurkse kolmnurga pindala.");
+
+		// Loob kaks objekti, et nende meetodeid kasutada.
 		Andmed obj = new Andmed();
 		Arvutused obj2 = new Arvutused();
+		
+		//Annab kaatetile ja kõrgusele väärtused.
 		obj.muudaKaatet1Vaartus(10);
 		obj.muudaKorgusVaartus(5);
+
+		// Arvutab täisnurkse kolmnurga pindala ja trükib välja.
 		obj2.muudaVastust(obj.tagastaKaatet1Vaartus() * obj.tagastaKorguseVaartus() / 2);
 		System.out.println("Antud taisnurkse kolmnurga pindala on " + obj2.annaVastus() + " Uhikut.");
+
+		// Muudab kaateti ja kõrguse väärtused ära.
 		obj.muudaKaatet1Vaartus(10);
 		obj.muudaKorgusVaartus(10);
+
+		// Arvutab täisnurkse kolmnurga pindala uuesti ja trükib välja.
 		obj2.muudaVastust(obj.tagastaKaatet1Vaartus() * obj.tagastaKorguseVaartus() / 2);
 		System.out.println("Antud taisnurkse kolmnurga pindala on " + obj2.annaVastus() + " Uhikut.");
 		
