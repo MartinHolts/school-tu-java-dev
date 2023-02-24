6bf347289136265e8d13b307eb54d7d89d0c85e9
MartinHoltsmeier <martinholtsmeier@gmail.com>
2023-02-24 16:53:34 +0200

    Commented files and changed file names. Also deleted one readme file that was not needed in the sub folder.

--- Tallinn_University-Lesson_3_data_class/Andmeproov.java
+++ Tallinn_University-Lesson_3_data_class/Andmeproov.java
@@ -1,4 +1,7 @@
 import java.io.*;
+
+// Otsib kommi, milles on kõige vähem süsivesinikke, kui uuring.minimum(5) väärus on 5, kuna kuues veerg on süsivesikud.
+// Indekseerimine hakkab 0st. Seega väärtus 5 tagastab 6-nda veeru.
 public class Andmeproov{
 	public static void main(String[] args) throws IOException{
 		Andmeuuring uuring=new Andmeuuring("http://www.tlu.ee/~jaagup/andmed/muu/kalevikommid.txt");
